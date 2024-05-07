5178079c0815fb2450a6a5aa0e8294f89eedd5b7
MyListon <myliston@gmail.com>
2024-05-07 00:38:09 -0300

    Implementação da redifinição de senha

--- src/main/java/com/soulcode/projetofinal/repositories/PersonRepository.java
+++ src/main/java/com/soulcode/projetofinal/repositories/PersonRepository.java
@@ -5,6 +5,7 @@ package com.soulcode.projetofinal.repositories;
 import com.soulcode.projetofinal.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
@@ -14,4 +15,7 @@ public interface PersonRepository extends JpaRepository<Person, Integer> {
 
     @Query("SELECT p FROM Person p WHERE p.name = :name")
     Person findByName(String name);
+
+    @Query("SELECT p FROM Person p WHERE p.resetToken = :resetToken")
+    Person findByResetToken(@Param("resetToken") String resetToken);
 }

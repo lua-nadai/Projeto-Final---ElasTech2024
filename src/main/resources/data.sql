e31112c388d1a76071c1021b68a0204c9ed14c47
MyListon <myliston@gmail.com>
2024-05-09 22:41:43 -0300

    Ajuste AuthenticationController e criação de um script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+use projeto_final;
+
+insert into type (name) values('Administrador'),('Técnico'),('Cliente');
+
+insert into department (name) values ('Financeiro'),('Operacional'),('Recursos Humanos'),('Administrativo'),('Comercial'),('Tecnologia da Informação');
+
+insert into status (name) values ('Aguardando Técnico'),('Em atendimento'),('Escalado para outro setor'),('Finalizado');
